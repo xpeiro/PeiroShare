580f9d958adbd5cffb70fbd1dcc8844fdb28721a
Daniel Peiró <danipeiro@gmail.com>
2015-12-13 01:58:41 +0100

    basic functionality prototype

--- app.js
+++ app.js
@@ -1,60 +1,71 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+/*  App Modules Setup   */
+const
+    express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    https = require('https'),
+    compression = require('compression'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    path = require('path');
+/*
+    Web Server Setup
+*/
+// Setup request compression (compress all)
+app.use(compression());
+// Set favicon
+app.use(favicon(__dirname + '/public/images/favicon.svg'));
+// Setup HTTP Request Logger (morgan).
+app.use(logger('dev', {
+    //do not log succesfull requests, only errors.
+    skip: function(req, res) {
+        return res.statusCode < 400
+    }
+}));
+// Set static route directory to ./public
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
+//Setup 404 error handler (if no handler before this is called, url not found)
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// Display error in html.
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.send('<h1>Error: ' + err.status + '<br>' + err.message + '</h1>' +
+        '<h3>Description: ' + req.url + ' was not found.</h3>');
 });
 
+var history = [];
+var currentdate = new Date();
 
-module.exports = app;
+// start server
+var privateKey  = fs.readFileSync('server.key', 'utf8');
+var certificate = fs.readFileSync('server.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials, app);
+io = require('socket.io')(httpsServer),
+httpsServer.listen(3000, function() {
+    console.log('PeiroShare: Server ready and listening on Port ' + 3000);
+});
+
+//socket io setup
+io.on('connection', function(newsocket) {
+    socket = newsocket;
+    currentdate = new Date();
+    console.log('Connected at ' + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds());
+    // log user disconnect
+    socket.on('disconnect', function() {
+    	currentdate = new Date();
+    	console.log('Disconnected at ' + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds());
+    });
+    socket.on('newText', function(data) {
+    	if (history.length == 5) {
+    		history.shift();
+    	};
+        history.push(data);
+        console.log(history);
+        socket.emit("newHistory", history);
+    });
+});
---
+++ public/javascripts/angularapp.js
@@ -0,0 +1,64 @@
+//app init
+var app = angular.module('PeiroShare', []);
+
+//main app controller. manages active modules, profiles and notifies back-end of change in controls when necessary.
+app.controller('PeiroShareController', ['$rootScope', '$scope',
+	function(rootScope, scope) {
+		var socket = io.connect({
+			secure: true
+		});
+		scope.text = "";
+		scope.send = function() {
+			socket.emit("newText", scope.text);
+
+		};
+		scope.copy = function(e, index) {
+			console.log(index);
+			var clip = new ClipboardEvent('copy');
+			clip.clipboardData.setData('text/plain', "testeo");
+		};
+		socket.on("newHistory", function(data) {
+			console.log(data);
+		});
+
+		scope.history = ["test1", "test2", "test3"];
+		scope.historyTable = generateHistoryTable(scope.history);
+
+		function generateHistoryTable(history) {
+			var historyTable = "<tr><th colspan=3>History</th></tr>";
+			for (var i = 0; i < history.length; i++) {
+				//begin row and element
+				historyTable = historyTable.concat("<tr><td>", i + 1, "</td><td>");
+				//add item
+				historyTable = historyTable.concat(history[i]);
+				//end element
+				historyTable = historyTable.concat("</td>");
+				//add copy button
+				historyTable = historyTable.concat("<td><button data-ng-click='copy($event," + i + ")''>Copy</button></td>");
+				//end row
+				historyTable = historyTable.concat("</tr>");
+			};
+			return historyTable;
+		};
+	},
+]);
+
+app.directive('compile', function($compile) {
+	return function(scope, element, attrs) {
+		scope.$watch(
+			function(scope) {
+				// watch the 'compile' expression for changes
+				return scope.$eval(attrs.compile);
+			},
+			function(value) {
+				// when the 'compile' expression changes
+				// assign it into the current DOM
+				element.html(value);
+
+				// compile the new DOM and link it to the current
+				// scope.
+				$compile(element.contents())(scope);
+			}
+		);
+	};
+});
